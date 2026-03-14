b5b4c9d850cdc4c8bdf44ce2445edb9a931fe9a2
jmata991 <jazielmata907@gmail.com>
2026-03-14 07:24:30 -0700

    Update customsounds.js

--- customsounds.js
+++ customsounds.js
@@ -1,3 +1,26 @@
-export const sounds =
-    // SPLITTER ---------------
-[
+// customsounds.js
+export const customSounds = [
+    {
+        name: "My Epic Sound",
+        mp3: "sounds/my-epic-sound.mp3", // Path to a local file in your folder
+        color: "#ff5733",
+        isCustom: true
+    }
+    // You can add more objects here manually as you get new files
+];
+import { sounds } from './sounds.js';
+import { customSounds as permanentCustoms } from './customsounds.js';
+
+function renderSounds(filter = '') {
+    soundBoard.innerHTML = '';
+
+    // Combine all three sources: 
+    // 1. Original sounds.js
+    // 2. The static customsounds.js you just made
+    // 3. Any dynamic sounds uploaded via the browser (localStorage)
+    const browserSaved = JSON.parse(localStorage.getItem('customSounds') || '[]');
+    
+    const allSounds = [...sounds, ...permanentCustoms, ...browserSaved];
+
+    // ... rest of your filtering and rendering logic ...
+}
